5ce586ab4ae3e260b405765d5fb3324f69a9ad5c
albertayeboah <albertayeboag@yahoo.com>
2025-03-10 13:34:13 +0000

    first commit

---
+++ src/pages/Books.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PagesLayout from "../layout/PagesLayout";
+
+
+const Books = () => {
+  return (
+    <PagesLayout>
+      <div>Books</div>
+    </PagesLayout>
+  );
+};
+
+export default Books;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PagesLayout from "../layout/PagesLayout";
+
+const Landing = () => {
+  return (
+    <PagesLayout>
+      <div>Landing</div>
+    </PagesLayout>
+   
+  );
+};
+
+export default Landing;
