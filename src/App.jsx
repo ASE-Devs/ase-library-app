4f871f67f334b413450c732bd46f94d371822656
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-11 11:50:01 +0000

    sample change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router"; 
+import { BrowserRouter, Route, Routes } from "react-router";
 import Books from "./pages/Books";
 import Landing from "./pages/Landing";
 import SingleBooks from "./pages/SingleBooks";
@@ -9,13 +9,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing/>} />  
+        <Route path="/" element={<Landing />} />
         <Route path="/books" element={<Books />} />
         <Route path="/single-book" element={<SingleBooks />} />
-        <Route path= "/add-book" element={<AddBooks />} />
+        <Route path="/add-book" element={<AddBooks />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// Comment
