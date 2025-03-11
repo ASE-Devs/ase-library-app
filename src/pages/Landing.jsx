76c5e1715445ae251903985f8ad99f254154d920
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-11 18:54:59 +0000

    update on main

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PagesLayout from "../layout/PagesLayout";
+import Newsletter from "../components/Newsletter";
 
 const Landing = () => {
   return (
     <PagesLayout>
       <div>Landing</div>
     </PagesLayout>
-   
+
   );
 };
 
