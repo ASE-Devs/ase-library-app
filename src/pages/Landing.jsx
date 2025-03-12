4dd84b6f3b1d245a0a649e165cdb8bf38d770057
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-12 09:30:53 +0000

    newsletter-and-bookcard

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import PagesLayout from "../layout/PagesLayout";
 import Newsletter from "../components/Newsletter";
+import Bookcard from "../components/Bookcard";
+
 
 const Landing = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
     <PagesLayout>
       <div>Landing</div>
+      <Newsletter />
+      <div className="flex gap-3 p-20">
+        {arr.map((ar) =>
+          <Bookcard key={ar} />
+        )}
+      </div>
+
+
     </PagesLayout>
 
   );
