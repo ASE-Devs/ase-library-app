7669b9900ee48892a24b8e107574797ba0ddd7c0
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-12 12:00:58 +0000

    add ons

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,12 +5,12 @@ import Bookcard from "../components/Bookcard";
 
 
 const Landing = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   return (
     <PagesLayout>
       <div>Landing</div>
       <Newsletter />
-      <div className="flex gap-3 p-20">
+      <div className="flex gap-6 p-20 flex-wrap ">
         {arr.map((ar) =>
           <Bookcard key={ar} />
         )}
