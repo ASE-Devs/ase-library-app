5455a57af9a94ed5f780b0f0f11c35b7a55adde3
Susanadjanie1 <susanadjanie1@gmail.com>
2025-03-13 05:18:38 +0000

    updated hero section nav links removed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 import PagesLayout from "../layout/PagesLayout";
-import HeroSection from "../components/HeroSection";
-import Newsletter from "../components/Newsletter";
-import HeroImage from "../assets/images/another.jpg"; // Local Image is imported here for each pages hero-section
+
 
 const Landing = () => {
   return (
     <PagesLayout>
-      {/* Props for Hero-Section with Button */}
-      <HeroSection
-        title="Discover Books That Read"
-        subtitle="Great reading experience"
-        image={HeroImage}
-        buttonText="Browse Now"
-      />
-
-      {/* Previous Sections in the main branch */}
-      <Newsletter />
     </PagesLayout>
   );
 };
